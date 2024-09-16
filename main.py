027599f5db4762930d2cf76004fabc8790ae6c73
ssolanes <ssolanes@uade.edu.ar>
2024-09-15 23:18:56 -0300

    Agrego validacion en main

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 from matriz_stock import stock_menu
 from ventas import ventas_menu
 from cliente import clientes_menu
+import validaciones
 
 matriz_stock = []
 matriz_ventas=[]
 matriz_clientes= []
 
-def __main__():
-    qmatriz = int(input("Ingrese la matriz que desea modificar o visualizar: 1 stock, 2 clientes, 3 ventas, 4 frenar el proceso: "))
-
-    if qmatriz == 1:
-        stock_menu(matriz_stock)
-    elif qmatriz == 2:
-        clientes_menu(matriz_clientes)
-    elif qmatriz == 3:
-        ventas_menu(matriz_ventas,matriz_stock,matriz_clientes)
-    elif qmatriz == 4:
-        return
+def __main__(): 
+    
+    qmatriz = input("Ingrese la matriz que desea modificar o visualizar: 1 stock, 2 clientes, 3 ventas, 4 frenar el proceso: ")
+    
+    if validaciones.vnumero(qmatriz):
+    
+        qmatriz=int(qmatriz)
+        if qmatriz == 1:
+            stock_menu(matriz_stock)
+        elif qmatriz == 2:
+            clientes_menu(matriz_clientes)
+        elif qmatriz == 3:
+            ventas_menu(matriz_ventas,matriz_stock,matriz_clientes)
+        elif qmatriz == 4:
+            return
+        else:
+            __main__()
+        __main__()
     else:
         __main__()
-    __main__()
-
 
 if __name__ == __main__():
     __main__()
