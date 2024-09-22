8dcb9c3ce58af39f4e775401903d72be9b1f5f7f
lbendersky <lbendersky@uade.edu.ar>
2024-09-21 21:27:47 -0300

    agregado de la funcion de resta de stock con venta

--- main.py
+++ main.py
@@ -3,31 +3,31 @@ from ventas import ventas_menu
 from cliente import clientes_menu
 import validaciones
 
-matriz_stock = []
-matriz_ventas=[]
-matriz_clientes= []
 
 def __main__(): 
     
-    qmatriz = input("Ingrese la matriz que desea modificar o visualizar: 1 stock, 2 clientes, 3 ventas, 4 frenar el proceso: ")
-    
-    if validaciones.vnumero(qmatriz):
-    
-        qmatriz=int(qmatriz)
-        if qmatriz == 1:
-            stock_menu(matriz_stock)
-        elif qmatriz == 2:
-            clientes_menu(matriz_clientes)
-        elif qmatriz == 3:
-            ventas_menu(matriz_ventas,matriz_clientes,matriz_stock)
-        elif qmatriz == 4:
-            return
-        else:
-            print("Ingrese un numero correcto")
-            __main__()
-        __main__()
-    else:
-        __main__()
+    matriz_stock = []
+    matriz_ventas=[]
+    matriz_clientes= []
+
+    flag = 0
+    while flag == 0:
+        qmatriz = input("Ingrese la matriz que desea modificar o visualizar: 1 stock, 2 clientes, 3 ventas, 4 frenar el proceso: ")
+        
+        if validaciones.vnumero(qmatriz):
+        
+            qmatriz=int(qmatriz)
+            if qmatriz == 1:
+                stock_menu(matriz_stock)
+            elif qmatriz == 2:
+                clientes_menu(matriz_clientes)
+            elif qmatriz == 3:
+                ventas_menu(matriz_ventas,matriz_clientes,matriz_stock)
+            elif qmatriz == 4:
+                flag = 1
+            else:
+                print("Ingrese un numero correcto")
+
 
 if __name__ == __main__():
     __main__()
--- ventas.py
+++ ventas.py
@@ -24,6 +24,7 @@ def ventas_menu(matriz_ventas,matriz_clientes,matriz_stock):
                     while band==0:
                         cantidad=input("Ingrese la cantidad vendida: ")
                         if validaciones.vnumero(cantidad):
+                            cantidad=int(cantidad)
                             band=1
                             
                     band=0
@@ -82,6 +83,7 @@ def ventas_menu(matriz_ventas,matriz_clientes,matriz_stock):
                             while band==0:
                                 datoacambiar=input("Ingrese el dato por el que desea cambiarlo: ")
                                 if validaciones.vnumero(datoacambiar):
+                                    datoacambiar = int(datoacambiar)
                                     band+=1
                                     
                         elif opcion==4:
@@ -93,7 +95,7 @@ def ventas_menu(matriz_ventas,matriz_clientes,matriz_stock):
                         else:
                             print("El número ingresado es incorrecto")
                             
-                    matriz_ventas=funciones.actualizarventas(matriz_ventas,pos,opcion,datoacambiar)
+                    matriz_ventas, matriz_stock = funciones.actualizarventas(matriz_ventas,pos,opcion,datoacambiar,matriz_stock)
                     
             elif num==4: 
                 
@@ -104,9 +106,9 @@ def ventas_menu(matriz_ventas,matriz_clientes,matriz_stock):
                         pos=input("Ingrese el ID que desea eliminar: ")
                         if validaciones.vnumero(pos):
                             pos=int(pos)
-                            if validaciones.viddic(matriz_clientes,pos):
+                            if validaciones.viddic(matriz_ventas,pos):
                                 band=1
-                    funciones.destruir_ventas(matriz_ventas,pos)
+                    funciones.destruir_ventas(matriz_ventas,pos,matriz_stock)
                     
             elif num==5:
                 flag = 1
